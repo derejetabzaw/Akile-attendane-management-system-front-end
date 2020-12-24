d20b3447a8c1053d1b0612d44e37d95298134765
helinagirmay <helinagirmay@gmail.com>
2020-12-24 14:06:00 +0300

    some changes on sites panel

--- src/views/users/User.js
+++ src/views/users/User.js
@@ -1,66 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import CIcon from "@coreui/icons-react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 import usersData from "./UsersData";
-// import * as mapboxgl from "react-mapbox-gl";
-// import "mapbox-gl/dist/mapbox-gl.css";
+import ReactMapGL from "react-map-gl";
+import "./MapViewerComp.css";
+import mapboxgl from "mapbox-gl";
 import PropTypes from "prop-types";
+import GoogleMapReact from "google-map-react";
 import { Tabs } from "antd";
+import Users from "./Users";
+import { render } from "enzyme";
 
 const { TabPane } = Tabs;
-// const Map = ReactMapboxGl({
-//   accessToken:
-//     "pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A",
-// });
 
-class User extends React.Component {
-  state = {
-    activeTab: "",
-  };
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-  setActiveTab = (tabKey) => {
-    this.setState({
-      active: tabKey,
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiaGVsaW5hLWciLCJhIjoiY2tqMDNiYXc1Mm4yMDJxc2NjbnVhdGNmZyJ9._r28TdYZ4-9oatr382Axsg";
+export default class User extends React.Component {
+  // const [viewPort, setViewPort] = useState({
+  //   latitude: 45.4211,
+  //   longitude: -75.6903,
+  //   width: "100vw",
+  //   height: "100vh",
+  //   zome: 10,
+  // });
+  constructor(props) {
+    super(props);
+    this.state = {
+      map: undefined,
+      lng: 45.4211,
+      lat: -75.6903,
+      zoom: 11,
+    };
+  }
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
     });
-  };
-
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+    this.setState({ map: map });
+  }
   render() {
     return (
       <>
         <Tabs defaultActiveKey="1">
           <TabPane tab="Add Site" key="1">
-            {/* <Map
-              style="mapbox://styles/mapbox/streets-v9"
-              containerStyle={{
-                height: "100vh",
-                width: "100vw",
-              }}
-            >
-              <Layer
-                type="symbol"
-                id="marker"
-                layout={{ "icon-image": "marker-15" }}
-              >
-                <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-              </Layer>
-            </Map> */}
+            {/* <ReactMapGL
+            {...viewPort}
+            // mapStyle="mapbox://styles/leighhalliday/cjufmjn1r2kic1fl9wxg7u1l4"
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            onViewportChange={(view) => {
+              console.log(view, "view");
+              setViewPort(view);
+            }}
+          ></ReactMapGL> */}
+            <div style={{ height: "100vh", width: "100%" }}>
+              <div>
+                <div
+                  ref={(el) => (this.mapContainer = el)}
+                  className="mapContainer"
+                />
+              </div>
+            </div>
           </TabPane>
           <TabPane tab="Assign Site" key="2">
-            Content of Tab Pane 2
+            <div style={{ width: "100%" }}>
+              <Users />
+            </div>
           </TabPane>
-          <TabPane tab="Inforamtion" key="3">
-            Content of Tab Pane 3
-          </TabPane>
+          <TabPane tab="Inforamtion" key="3"></TabPane>
         </Tabs>
       </>
     );
   }
 }
+// }
+// AIzaSyBqSabtygfHfwwG9W9g_-qrWP-8r6q-zgk
+// <script defer
+//     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDRb886yC1brzjRDS-UtSPAB9DJpFQxbRY&callback=initMap">
+// </script>
 
-User.propTypes = {
-  google: PropTypes.object,
-};
-
-export default GoogleApiWrapper({
-  apiKey: "YOUR_GOOGLE_API_KEY_GOES_HERE",
-})(User);
+// export default GoogleApiWrapper({
+//   apiKey: "AIzaSyBqSabtygfHfwwG9W9g_-qrWP-8r6q-zgk",
+// })(User);
--- src/views/users/Users.js
+++ src/views/users/Users.js
@@ -1,86 +1,199 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
-import {
-  CBadge,
-  CCard,
-  CCardBody,
-  CCardHeader,
-  CCol,
-  CDataTable,
-  CRow,
-  CPagination
-} from '@coreui/react'
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { Col, Row, Tabs, Button } from "antd";
+import { Draggable, DragDropContext, Droppable } from "react-beautiful-dnd";
+import usersData from "./UsersData";
+import { v4 as uuid } from "uuid";
+import Column from "antd/lib/table/Column";
+import { Provider } from "react-redux";
+// import { copyWithin } from "core-js/fn/array";
 
-import usersData from './UsersData'
+const { TabPane } = Tabs;
 
-const getBadge = status => {
-  switch (status) {
-    case 'Active': return 'success'
-    case 'Inactive': return 'secondary'
-    case 'Pending': return 'warning'
-    case 'Banned': return 'danger'
-    default: return 'primary'
+const itemsOfData = [
+  { id: uuid(), content: "First" },
+  { id: uuid(), content: "Second" },
+];
+
+const columnData = {
+  [uuid()]: {
+    name: "Employee",
+    items: itemsOfData,
+  },
+  [uuid()]: {
+    name: "Sites",
+    items: [],
+  },
+};
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+  if (source.droppableId !== destination.droppableId) {
+    const sourceCol = columns[source.droppableId];
+    const destinartionCol = columns[source.droppableId];
+    const sourceItems = [...sourceCol.items];
+    const destiantionItems = [...destinartionCol.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destiantionItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceCol,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destinartionCol,
+        items: destiantionItems,
+      },
+    });
+  } else {
+    const col = columns[source.droppableId];
+    const copiedItem = [...col.items];
+    const [removed] = copiedItem.splice(source.index, 1);
+    copiedItem.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...col,
+        items: copiedItem,
+      },
+    });
   }
-}
+};
 
 const Users = () => {
-  const history = useHistory()
-  const queryPage = useLocation().search.match(/page=([0-9]+)/, '')
-  const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1)
-  const [page, setPage] = useState(currentPage)
+  // const history = useHistory();
+  // const queryPage = useLocation().search.match(/page=([0-9]+)/, "");
+  // const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1);
+  // const [page, setPage] = useState(currentPage);
 
-  const pageChange = newPage => {
-    currentPage !== newPage && history.push(`/users?page=${newPage}`)
-  }
+  // const pageChange = (newPage) => {
+  //   currentPage !== newPage && history.push(`/users?page=${newPage}`);
+  // };
 
-  useEffect(() => {
-    currentPage !== page && setPage(currentPage)
-  }, [currentPage, page])
+  // useEffect(() => {
+  //   currentPage !== page && setPage(currentPage);
+  // }, [currentPage, page]);
+
+  const [columns, setColumns] = useState(columnData);
 
   return (
-    <CRow>
-      <CCol xl={6}>
-        <CCard>
-          <CCardHeader>
-            Users
-            <small className="text-muted"> example</small>
-          </CCardHeader>
-          <CCardBody>
-          <CDataTable
-            items={usersData}
-            fields={[
-              { key: 'name', _classes: 'font-weight-bold' },
-              'registered', 'role', 'status'
-            ]}
-            hover
-            striped
-            itemsPerPage={5}
-            activePage={page}
-            clickableRows
-            onRowClick={(item) => history.push(`/users/${item.id}`)}
-            scopedSlots = {{
-              'status':
-                (item)=>(
-                  <td>
-                    <CBadge color={getBadge(item.status)}>
-                      {item.status}
-                    </CBadge>
-                  </td>
-                )
-            }}
-          />
-          <CPagination
-            activePage={page}
-            onActivePageChange={pageChange}
-            pages={5}
-            doubleArrows={false} 
-            align="center"
-          />
-          </CCardBody>
-        </CCard>
-      </CCol>
-    </CRow>
-  )
-}
+    <>
+      <Row>
+        <Col>
+          <div
+            style={{ display: "flex", justifyContent: "center", height: "100" }}
+          >
+            <DragDropContext
+              onDragEnd={(result) =>
+                // console.log("iiii", result)
+                onDragEnd(result, columns, setColumns)
+              }
+            >
+              <Tabs tabPosition="left">
+                {/* <Row>
+                  <Col> */}
+                <TabPane tab="Site 1" key="1">
+                  {Object.entries(columns).map(([id, column]) => {
+                    return (
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          alignItems: "center",
+                        }}
+                      >
+                        <h3>{column.name} </h3>
+                        <div style={{ margin: 8 }}>
+                          <Droppable droppableId={id}>
+                            {(provided, snapshot) => {
+                              return (
+                                <div
+                                  {...provided.droppableProps}
+                                  ref={provided.innerRef}
+                                  style={{
+                                    background: snapshot.isDraggingOver
+                                      ? "white"
+                                      : "lightgray",
+                                    padding: 4,
+                                    width: 250,
+                                    minHeight: 500,
+                                  }}
+                                >
+                                  {column.items.map((item, index) => {
+                                    return (
+                                      <Draggable
+                                        key={item.key}
+                                        draggableId={item.id}
+                                        index={index}
+                                      >
+                                        {(provided, snapshot) => {
+                                          return (
+                                            <div
+                                              ref={provided.innerRef}
+                                              {...provided.draggableProps}
+                                              {...provided.dragHandleProps}
+                                              style={{
+                                                userSelect: "none",
+                                                margin: "0 0 8px 0",
+                                                padding: 16,
+                                                minHeight: "50px",
+                                                backgroundColor: snapshot.isDragging
+                                                  ? "#263B4A"
+                                                  : "#456C86",
+                                                color: "white",
+                                                ...provided.draggableProps
+                                                  .style,
+                                              }}
+                                            >
+                                              {item.content}
+                                            </div>
+                                          );
+                                        }}
+                                      </Draggable>
+                                    );
+                                  })}
+                                </div>
+                              );
+                            }}
+                          </Droppable>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </TabPane>
+                {/* </Col>
+                </Row> */}
+              </Tabs>
+            </DragDropContext>
+          </div>
+        </Col>
+        <Col style={{ float: "right", marginLeft: "45%" }}>
+          {" "}
+          <Button ghost>Save</Button>
+        </Col>
+      </Row>
+      {/* <Row>
+        <Col span={12}>
+          <Tabs tabPosition="left">
+            <TabPane tab="Tab 1" key="1">
+              <p>Bole Site </p>
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+              <p>Bole Site2 </p>
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+              <p>Bole Site3 </p>
+            </TabPane>
+          </Tabs>
+        </Col>
+        <Col span={12}>
+          <p>ppppp</p>
+        </Col>
+      </Row> */}
+    </>
+  );
+};
 
-export default Users
+export default Users;
