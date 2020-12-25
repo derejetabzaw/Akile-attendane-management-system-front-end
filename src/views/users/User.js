75962e15c0aec02d7bbf0a278e01d03c8c0af29e
helinagirmay <helinagirmay@gmail.com>
2020-12-25 17:27:57 +0300

    fixes on map

--- src/views/users/User.js
+++ src/views/users/User.js
@@ -20,15 +20,37 @@ const { TabPane } = Tabs;
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function User() {
+  const [mapLayers, setMapLayer] = useState([]);
+
   const _onCreate = (e) => {
     console.log(e, "onCreate");
+
+    const { layerType, layer } = e;
+    if (layerType === "marker") {
+      const { _leaflet_id } = layer;
+
+      setMapLayer((layer) => [...layer, { id: _leaflet_id }]);
+      console.log(layer, "layersss");
+    }
   };
+
   const _onEditPath = (e) => {
     console.log(e, "edit path");
+    let numEdited = 0;
+    e.layers.eachLayer((layer) => {
+      numEdited += 1;
+    });
+    console.log(`_onEdited: edited ${numEdited} layers`, e);
   };
 
   const _onDeleted = (e) => {
     console.log(e, "deleted");
+
+    let numDeleted = 0;
+    e.layers.eachLayer((layer) => {
+      numDeleted += 1;
+    });
+    console.log(`onDeleted: removed ${numDeleted} layers`, e);
   };
 
   return (
@@ -48,22 +70,25 @@ export default function User() {
               />
               <Marker position={[8.980603, 38.757759]}>
                 <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
+                  <FeatureGroup>
+                    <EditControl
+                      position="topright"
+                      onEdited={_onEditPath}
+                      onCreated={_onCreate}
+                      onDeleted={_onDeleted}
+                      draw={{
+                        rectangle: false,
+                        circle: false,
+                        polyline: false,
+                        polygone: false,
+                      }}
+                    />
+                    <Circle center={[51.51, -0.06]} radius={200} />
+                  </FeatureGroup>
                 </Popup>
               </Marker>
-              <FeatureGroup>
-                <EditControl
-                  position="topright"
-                  onEdited={_onEditPath}
-                  onCreated={_onCreate}
-                  onDeleted={_onDeleted}
-                  draw={{
-                    rectangle: false,
-                  }}
-                />
-                <Circle center={[51.51, -0.06]} radius={200} />
-              </FeatureGroup>
             </Map>
+            <pre>{JSON.stringify(mapLayers, 0, 2)}</pre>
           </div>
         </TabPane>
         <TabPane tab="Assign Site" key="2">
