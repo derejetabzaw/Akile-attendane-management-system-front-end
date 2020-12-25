f2d17c16f14df234c79738d10e6450cec2b0741a
helinagirmay <helinagirmay@gmail.com>
2020-12-25 16:50:09 +0300

    usser pin on map

--- src/views/users/User.js
+++ src/views/users/User.js
@@ -1,22 +1,42 @@
 import React, { useState } from "react";
 import usersData from "./UsersData";
 import "./MapViewerComp.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  Map,
+  TileLayer,
+  Marker,
+  Popup,
+  FeatureGroup,
+  Circle,
+} from "react-leaflet";
+import L from "leaflet";
+import { EditControl } from "./src";
+// import { EditControl } from "react-leaflet-draw";
 import { Tabs } from "antd";
 import Users from "./Users";
-import useSwr from "swr";
 
 const { TabPane } = Tabs;
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function User() {
+  const _onCreate = (e) => {
+    console.log(e, "onCreate");
+  };
+  const _onEditPath = (e) => {
+    console.log(e, "edit path");
+  };
+
+  const _onDeleted = (e) => {
+    console.log(e, "deleted");
+  };
+
   return (
     <>
       <Tabs defaultActiveKey="1">
         <TabPane tab="Add Site" key="1">
           <div style={{ height: "82vh", width: "100%" }}>
-            <MapContainer
+            <Map
               center={[8.980603, 38.757759]}
               zoom={12}
               scrollWheelZoom={false}
@@ -31,7 +51,19 @@ export default function User() {
                   A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
               </Marker>
-            </MapContainer>
+              <FeatureGroup>
+                <EditControl
+                  position="topright"
+                  onEdited={_onEditPath}
+                  onCreated={_onCreate}
+                  onDeleted={_onDeleted}
+                  draw={{
+                    rectangle: false,
+                  }}
+                />
+                <Circle center={[51.51, -0.06]} radius={200} />
+              </FeatureGroup>
+            </Map>
           </div>
         </TabPane>
         <TabPane tab="Assign Site" key="2">
